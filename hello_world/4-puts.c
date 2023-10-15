ca2a8c5fcbed36f96d7a3cfc8da91439809a97d8
Moussaldhino93 <moussa.gory@icloud.com>
2023-10-14 19:57:37 -0700

    4-puts.c

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,10 +1,18 @@
 #include <stdio.h>
+
+
+/**
+ * main - puts 'Programming is like building a multilingual puzzle'
+ * Return: Always 0.
+ */
+
+
 int main(void)
 
 
 {
 	puts("Programming is like building a multilingual puzzle\n");
-	return (0)
+	return (0);
 
 
 }		
