cf9476d3f827a608218e74e69bbe12e36d73fe70
Moussaldhino93 <moussa.gory@icloud.com>
2023-10-16 09:16:20 -0700

    1-last_digit.c

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -2,26 +2,29 @@
 #include <time.h>
 #include <stdio.h>
 
-/* more headers goes there */
+/**
+ * main - printf The string Last digit of
+ * Return: Always 0
+ */
 
-/* betty style doc for function main goes there */
 int main(void)
 {
 		int n;
 
 		srand(time(0));
 		n = rand() - RAND_MAX / 2;
-		if ( n > 5 )
+		if (n > 5)
 		{
 			printf("%dThe string is positive\n", n);
 		}
-		else if (n == 0 )
-		{
-			printf("%dThe string is 0\n", n);
-		}
-		else
+		else if (n < 0)
 		{
 			printf("%dThe string is negative\n", n);
+		}
+		else
+		(n == 0);
+		{
+			printf("%dThe string is posititve\n", n);
 		}
 		return (0);
 }
