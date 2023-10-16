7a7ed28c37df67bdca75328be715f2fb03640683
Moussaldhino93 <moussa.gory@icloud.com>
2023-10-16 05:02:03 -0700

    0-positive_or_negative.c

--- variables_if_else_while/0-positive_or_negative.c
+++ variables_if_else_while/0-positive_or_negative.c
@@ -4,7 +4,7 @@
 
 /* betty style doc for function main goes there */
 
-	int main(void)
+int main(void)
 {
 	int n;
 	srand(time(0));
@@ -13,11 +13,11 @@
 	if (n < 0)
 	{
 	printf("%d is negative\n", n);
-	}	
+	}
 	else if (n == 0)
 	{
 	printf("%d is zero\n", n);
-	}	
+	}
 	else
 	{
 	printf("%d is positive\n", n);
