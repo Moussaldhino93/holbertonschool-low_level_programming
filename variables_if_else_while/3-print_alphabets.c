9121f0424aafca61492ddb9fedc009c55c4e78ec
Moussaldhino93 <moussa.gory@icloud.com>
2023-10-17 09:50:55 -0700

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,21 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - putchar prints the alphabet in lowercase, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
+	for (l = 'A'; l <= 'Z'; l++)
+		putchar (l);
+	putchar ('\n');
+	return (0);
+}
