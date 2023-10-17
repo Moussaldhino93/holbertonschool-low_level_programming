8fea3dd89b0c647276972677d9a0be4117175ea3
Moussaldhino93 <moussa.gory@icloud.com>
2023-10-17 16:41:45 -0700

    9-print_comb.c

--- variables_if_else_while/9-print_comb.c
+++ variables_if_else_while/9-print_comb.c
@@ -10,12 +10,15 @@
 int main(void)
 {
 	int n;
-	for (n = '0'; n <= '9'; n++){
+
+	for (n = 0; n <= 9; n++)
+	{
 		putchar(n + '0');
 
-		if (n < 9){
-			putchar(',');
-			putchar(' ');
+	if (n < 9)
+	{
+		putchar(',');
+		putchar(' ');
 		}
 	}
 
