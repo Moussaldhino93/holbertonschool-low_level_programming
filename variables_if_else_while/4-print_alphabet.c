4a2f95dc7e0bedf74e27403bf464eba49737a86f
Moussaldhino93 <moussa.gory@icloud.com>
2023-10-17 10:02:37 -0700

    4-print_alphabet.c

---
+++ variables_if_else_while/4-print_alphabet.c
@@ -0,0 +1,22 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - putchar prints the alphabet in lowercase, followed by a new line.
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+
+	char l;
+
+	for (l = 'a'; l <= 'z'; l++)
+		putchar(l);
+	if (l != 'e' != 'q')
+		putchar(l);
+
+	putchar ('\n');
+	return (0);
+}
