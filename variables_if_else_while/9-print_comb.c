3e7b5461663d1705d3fef84bfe3249e34fd686cf
Moussaldhino93 <moussa.gory@icloud.com>
2023-10-17 15:52:51 -0700

    9-print_comb.c

---
+++ variables_if_else_while/9-print_comb.c
@@ -0,0 +1,25 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+ * main - prints all possible combinations of single-digit numbers
+ * Rerurn: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	for (n = 0; n < 10; n++)
+		putchar(n + '0');
+	if (n < 9)
+		putchar(',');
+		putchar(' ');
+
+
+	putchar('\n')
+	return (0);
+}
