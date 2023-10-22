be729db145597631ff15c4cbaf4897d5b1fe8586
Moussaldhino93 <moussa.gory@icloud.com>
2023-10-22 08:30:17 -0700

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -18,7 +18,7 @@ void jack_bauer(void)
 	{
 	for (d = 0; d <= 9; d++)
 	{
-																							_putchar(a + '0');
+		_putchar(a + '0');
 		_putchar(b + '0');
 		_putchar(58);
 		_putchar(c + '0');
