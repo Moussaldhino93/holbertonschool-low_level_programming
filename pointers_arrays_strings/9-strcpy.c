41bbbf413ca507f3249fd1f434e07b7a9d8cb05a
Moussaldhino93 <moussa.gory@icloud.com>
2023-10-26 16:00:08 -0700

    9-strcpy.c

---
+++ pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_strcpy - copy the string pointed by src
+ * including the terminal null byte (\0)
+ * @dest: destination
+ * @src: source
+ * Return: The pointer to dest
+ */
+
+char *_strcpy(char *dest, char *src)
+{
+	int count = 0;
+
+	while (count >= 0)
+	{
+	*(dest + count) = *(src + count);
+	if (*(src + count) == '\0')
+		break;
+	count++;
+	}
+	return (dest);
+}
