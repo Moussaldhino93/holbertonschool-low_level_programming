33b03ae7a17811a836f26233602538ecce9d9e29
Moussaldhino93 <moussa.gory@icloud.com>
2023-11-01 05:13:46 -0700

    2-strlen_recursion.c

--- recursion/2-strlen_recursion.c
+++ recursion/2-strlen_recursion.c
@@ -4,22 +4,15 @@
 
 /**
  * _strlen_recursion - Returns the length of a string
- * @s: 
+ * @s: The string
  * Return: Always 0
  */
 
 int _strlen_recursion(char *s)
 {
-	char s [ 100 ];
-
-	printf ( "%d \n " , string_length (s));
-	return 0;
-}
-
-int _strlen_recursion(char *s) 
-{
-	if ( * s == ' \0 ' )
-	return 0;
-
-	return (1 + string_length (s++));
+	if (*s == '\0')
+	{
+	return (0);
+	}
+	_strlen_recursion(s + 1);
 }
