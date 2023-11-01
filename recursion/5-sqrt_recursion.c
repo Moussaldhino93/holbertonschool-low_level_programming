e3ca0df185565ac41654905ae9127b0ffa19fa8a
Moussaldhino93 <moussa.gory@icloud.com>
2023-11-01 13:22:03 -0700

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -13,13 +13,5 @@ int _sqrt_recursion(int n)
 	{
 	return -1;
 	}
-
-	sqrt((double n));
-	if (n == 0)
-	{
-	return (0);
-	}
-	else
-	{
-	return(_sqrt_recursion(n - 1));	
+	return(_sqrt_recursion(n, 1));	
 }
