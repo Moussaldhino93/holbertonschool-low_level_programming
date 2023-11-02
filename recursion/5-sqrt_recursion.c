4e4208b8b5a9b59f9d1251fe571bfb472408494f
Moussaldhino93 <moussa.gory@icloud.com>
2023-11-02 01:47:43 -0700

    5-sqrt_recursion.c

--- recursion/5-sqrt_recursion.c
+++ recursion/5-sqrt_recursion.c
@@ -8,11 +8,19 @@
  * Return: The natural square root of a number
  */
 
-int _sqrt_recursion(int n)
+int _sqrt_recursion(int n, int i)
 {
 	if (n < 0)
 	{
 	return (-1);
 	}
-	return (_sqrt_recursion(n / 2));
+	if ( i * i > n)
+	{
+	return (-1);
+	}
+	if (i * i == n)
+	{
+	return (i);
+	}
+	return (_sqrt_recursion(n, i + 1));
 }
