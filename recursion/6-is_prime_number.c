ee39ba44bf2c5533eec0674badbd420fa6a26c69
Moussaldhino93 <moussa.gory@icloud.com>
2023-11-02 11:22:22 -0700

    6-is_prime_number.c

--- recursion/6-is_prime_number.c
+++ recursion/6-is_prime_number.c
@@ -19,3 +19,18 @@ int is_prime_integer(int n, int integer)
 	}
 	return (is_prime_integer(n, integer - 1));
 }
+
+/**
+ * is_prime_number - Return if the input is a prime number
+ * @n: The parameter
+ * Return: 1 for prime otherwise return 0
+ */
+
+int is_prime_number(int n)
+{
+	if (n <= 1)
+	{
+	return (0);
+	}
+	return (is_prime_integer(n, n - 1));
+}
