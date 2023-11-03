e17aeb720ae578b92b48ad747b0f44fe8e60eee7
Moussaldhino93 <moussa.gory@icloud.com>
2023-11-03 01:52:24 -0700

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,7 +1,13 @@
 #include <stdio.h>
+/**
+ * principal - prints its name, followed by a new line.
+ * @argc: 
+ * @argv:
+ * Return: 0
+ */
 
 int principal (int argc, char *argv[])
 {
-	printf("%s\n", *argv++);
+	printf("%s\n", argv[]);
 	return (0) ;
 }
